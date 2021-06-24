b44f164d7734158d2f7cc36d55f43dbadae752a4
sohey-dr <heyso1209@gmail.com>
2021-06-24 16:54:15 +0900

    bigger form

--- components/Form.vue
+++ components/Form.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="w-full max-w-xs">
+    <div class="w-full max-w-md">
       <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div class="mb-4">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="youtube_url">
