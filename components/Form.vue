59f1a1e8e17022868e00f01a412f3610d584798a
sohey-dr <heyso1209@gmail.com>
2021-06-24 16:51:51 +0900

    Cleaen form

--- components/Form.vue
+++ components/Form.vue
@@ -6,13 +6,13 @@
           <label class="block text-gray-700 text-sm font-bold mb-2" for="youtube_url">
             YoutubeのURL
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="youtube_url" type="text" placeholder="Username">
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="youtube_url" type="text" placeholder="https://www.youtube.com/hoge">
         </div>
         <div class="mb-6">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="picture_url">
             写真のURL
           </label>
-          <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="picture_url" type="picture_url" placeholder="https:picture.com">
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="picture_url" type="picture_url" placeholder="https:picture.com">
         </div>
         <div class="flex items-center justify-between">
           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
