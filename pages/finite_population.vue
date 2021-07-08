d56971781f047d021eee5a352c0c3a3bac15b96e
sohey-dr <heyso1209@gmail.com>
2021-07-08 18:16:38 +0900

    Clean

--- pages/finite_population.vue
+++ pages/finite_population.vue
@@ -2,13 +2,48 @@
   <div class="container mx-auto my-60 w-full max-w-xs">
     <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
       <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        <label
+          class="block text-gray-700 text-sm font-bold mb-2"
+          for="username"
+        >
           調査対象の母集団の数
         </label>
-        <input v-model="populationNumber" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="数を入力">
+        <input
+          v-model="populationNumber"
+          class="
+            shadow
+            appearance-none
+            border
+            rounded
+            w-full
+            py-2
+            px-3
+            text-gray-700
+            leading-tight
+            focus:outline-none
+            focus:shadow-outline
+          "
+          id="username"
+          type="text"
+          placeholder="数を入力"
+        />
       </div>
       <div class="w-28 mx-auto flex items-center justify-between">
-        <button @click="calculation({populationNumber})" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        <button
+          @click="calculation({ populationNumber })"
+          class="
+            bg-blue-500
+            hover:bg-blue-700
+            text-white
+            font-bold
+            py-2
+            px-4
+            rounded
+            focus:outline-none
+            focus:shadow-outline
+          "
+          type="button"
+        >
           計算する
         </button>
       </div>
@@ -19,7 +54,7 @@
 <script>
 export default {
   data: {
-    populationNumber: null
+    populationNumber: null,
   },
   methods: {
     calculation({ populationNumber }) {
@@ -29,8 +64,8 @@ export default {
       var children = f_child * f_child * s_child + 1
       var answer = populationNumber / children
 
-      window.alert(Math.round(answer) + "人(個)程度必要です")
-    }
-  }
+      window.alert(Math.round(answer) + '人(個)程度必要です')
+    },
+  },
 }
 </script>
