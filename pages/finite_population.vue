f22cc36ae3e0df52de8eaecf73a75b59d493f0fc
sohey-dr <heyso1209@gmail.com>
2021-07-08 18:16:15 +0900

    Add finite_population

---
+++ pages/finite_population.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mx-auto my-60 w-full max-w-xs">
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+          調査対象の母集団の数
+        </label>
+        <input v-model="populationNumber" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="数を入力">
+      </div>
+      <div class="w-28 mx-auto flex items-center justify-between">
+        <button @click="calculation({populationNumber})" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          計算する
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
