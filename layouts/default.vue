11895415dab9a08aa1136f7bcb8bd4c5b9d8db83
soheyHashimoto <heyso1209@gmail.com>
2021-11-03 22:37:38 +0900

    [ADD]headerとfooter

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
+    <Header />
     <Nuxt />
+    <Footer />
   </div>
 </template>
+
+<script>
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
 
 <style>
 html {
