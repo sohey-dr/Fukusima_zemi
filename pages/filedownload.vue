8447e59db095702fe08ae260eba5d6daed74cb3a
soheyHashimoto <heyso1209@gmail.com>
2021-11-04 15:36:15 +0900

    [ADD]filedownloadでファイルをダウンロードできるようにする

---
+++ pages/filedownload.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="my-page">
+    <button v-on:click="downloadFile()">ダウンロードする</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    async downloadFile() {
+      await this.$axios.$post("http://localhost:8080/", { responseType: 'blob' })
+        .then(response => {
+          console.log(response);
+          const blob = new Blob([response], { type: response.type })
+          const link = document.createElement('a')
+          link.href = URL.createObjectURL(blob)
+          link.download = "sphero.ts";
+          link.click()
+          URL.revokeObjectURL(link.href)
+        }).catch(console.error)
+    }
+  },
+}
+</script>
